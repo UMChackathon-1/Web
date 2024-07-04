4d3b6ff8edbe0c9759ebfd1ddf7126be3287c364
gs0428 <lider28@naver.com>
2024-07-04 21:56:27 +0900

    refactor: bottom nav 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "@pages/Home";
 import NotFound from "@pages/404";
-import Community from "@pages/Community";
 import Profile from "@pages/Profile";
-import Layout from "@layout/Layout";
 
 function App() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/profile" element={<Profile />} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,3 @@
-import BottomNavigation from "@components/BottomNavigation";
-
 export default function Home() {
-  return (
-    <>
-      <h1>This is Home.</h1>
-      <BottomNavigation />
-    </>
-  );
+  return <h1>This is Home.</h1>;
 }
