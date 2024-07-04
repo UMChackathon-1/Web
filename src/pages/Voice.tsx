c82cf99b418c814b8965cde0ad2a986b68672748
JJHyeok13 <jjinh99@naver.com>
2024-07-05 05:36:33 +0900

    design: Voice 페이지 디자인 수정

--- src/pages/Voice.tsx
+++ src/pages/Voice.tsx
@@ -15,9 +15,9 @@ export default function Voice() {
   };
 
   return (
-    <main className="px-4">
+    <main>
       <BackButton />
-      <section className="flex flex-col gap-2 mt-5 mb-12">
+      <section className="flex flex-col gap-2 px-4 mt-5 mb-12">
         <h1 className="text-lg font-bold text-[#BE9EFF]">내용 입력하기</h1>
         <textarea
           placeholder="입력한 내용을 TTS가 음성으로 말해줍니다."
@@ -26,10 +26,15 @@ export default function Voice() {
           className="border border-[#8248F5] w-full min-h-60 bg-[rgba(255,255,255,0.1)] rounded-md px-5 py-4 outline-none text-[rgba(255,255,255,0.5)] placeholder:text-[rgba(255,255,255,0.5)]"
         />
       </section>
-      <button onClick={handleSpeadk} className="text-white py-6 bg-[rgba(255,255,255,0.2)] flex flex-col w-full items-center gap-2 rounded-md outline-none">
-        <MicSvg color="white" />
-        생성한 오디오를 들어보세요
-      </button>
+      <div className="px-4">
+        <button
+          onClick={handleSpeadk}
+          className="text-white py-6 bg-[rgba(255,255,255,0.2)] flex flex-col w-full items-center gap-2 rounded-md outline-none"
+        >
+          <MicSvg color="white" />
+          생성한 오디오를 들어보세요
+        </button>
+      </div>
     </main>
   );
 }
