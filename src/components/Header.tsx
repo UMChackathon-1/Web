c44f8b4865eec9e3233eafd526088050f16eebba
gs0428 <lider28@naver.com>
2024-07-05 01:54:02 +0900

    feat: logo 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import ProfileSvg from "@assets/profile.svg";
+import Logo from "@assets/logo.svg";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-4 mb-9">
       <Link to="/">
-        <h1 className="text-[#6B6AFF] text-xl font-semibold">Party Tonight</h1>
+        <Logo />
       </Link>
       <Link to="/profile">
         <ProfileSvg color="#6B6AFF" />
