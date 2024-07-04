c97b9f44b9b156376ea6b78e0c0bec0694919a92
gs0428 <lider28@naver.com>
2024-07-05 01:17:48 +0900

    feat: TTS 화면 및 기능 구현

--- src/pages/Voice.tsx
+++ src/pages/Voice.tsx
@@ -1,3 +1,38 @@
+import ArrowLeft from "@assets/arrow-left.svg";
+import MicSvg from "@assets/mic.svg";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import speak from "utils/speak";
+
 export default function Voice() {
-  return <h1>Voice</h1>;
+  const [text, setText] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleSpeadk = () => {
+    speak(text);
+  };
+
+  return (
+    <main className="px-4">
+      <Link to="/">
+        <ArrowLeft color="#FFFFFF" />
+      </Link>
+      <section className="flex flex-col gap-2 mt-5 mb-12">
+        <h1 className="text-lg font-bold text-[#BE9EFF]">내용 입력하기</h1>
+        <textarea
+          placeholder="입력한 내용을 TTS가 음성으로 말해줍니다."
+          value={text}
+          onChange={handleChange}
+          className="border border-[#8248F5] w-full min-h-60 bg-[rgba(255,255,255,0.1)] rounded-md px-5 py-4 outline-none text-[rgba(255,255,255,0.5)] placeholder:text-[rgba(255,255,255,0.5)]"
+        />
+      </section>
+      <button onClick={handleSpeadk} className="text-white py-6 bg-[rgba(255,255,255,0.2)] flex flex-col w-full items-center gap-2 rounded-md outline-none">
+        <MicSvg color="white" />
+        생성한 오디오를 들어보세요
+      </button>
+    </main>
+  );
 }
