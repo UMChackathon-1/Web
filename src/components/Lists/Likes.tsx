68b9a36c559aafa769a49416c815b386baf09c7b
gs0428 <lider28@naver.com>
2024-07-05 05:14:52 +0900

    feat: 더미데이터 추가

--- src/components/Lists/Likes.tsx
+++ src/components/Lists/Likes.tsx
@@ -2,24 +2,16 @@ import ThumbsUpSvg from "@assets/thumbs-up-gray.svg";
 import ThumbsDownSvg from "@assets/thumbs-down-gray.svg";
 import DownloadSvg from "@assets/download-gray.svg";
 
-export const Likes = ({
-  likeCount,
-  dislikeCount,
-  downloadCount,
-}: {
-  likeCount: number;
-  dislikeCount: number;
-  downloadCount: number;
-}) => {
+export const Likes = ({ likes, dislikeCount, downloadCount }: { likes?: number; dislikeCount?: number; downloadCount?: number }) => {
   const buttons = [
-    { num: likeCount, svg: ThumbsUpSvg },
+    { num: likes, svg: ThumbsUpSvg },
     { num: dislikeCount, svg: ThumbsDownSvg },
     { num: downloadCount, svg: DownloadSvg },
   ];
   return (
     <div className="flex gap-2 my-3">
-      {buttons.map((item) => (
-        <div className="flex flex-col items-center gap-1">
+      {buttons.map((item, idx) => (
+        <div key={idx} className="flex flex-col items-center gap-1">
           <item.svg />
           <div className="text-[14px] text-[#9797A8]">{item.num}</div>
         </div>
