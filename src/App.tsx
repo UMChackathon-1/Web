73af2bd94bbd5613aa6c100f14ad2ecfbdd59dff
JJHyeok13 <jjinh99@naver.com>
2024-07-04 16:01:17 +0900

    chore: 절대 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/404";
+import Home from "@pages/Home";
+import NotFound from "@pages/404";
 
 function App() {
   return (
