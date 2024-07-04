9589365dce2ef7c11cafb439fd826bb654212fd8
JJHyeok13 <jjinh99@naver.com>
2024-07-04 21:31:34 +0900

    feat: 404 페이지 메인돌아가기 링크 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <h1>404: Not Found</h1>;
+  return (
+    <>
+      <h1>404: Not Found</h1>
+      <Link to="/">메인으로 돌아가기</Link>
+    </>
+  );
 }
