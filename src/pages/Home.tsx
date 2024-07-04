827b0a625bf16e0d15e086ee324fb18d4afcc2a0
gs0428 <lider28@naver.com>
2024-07-05 05:16:41 +0900

    feat: 홈 화면 데이터 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import MicSvg from "@assets/mic.svg";
 import Title from "@components/home/Title";
-import outdoorSleep1 from "@assets/outdoor-sleep1.png";
-import outdoorSleep2 from "@assets/outdoor-sleep2.png";
-import outdoorSleep3 from "@assets/outdoor-sleep3.png";
 import Card from "@components/home/Card";
 import speak from "utils/speak";
 import Header from "@components/Header";
 import PostItems from "@components/community/postItems";
 import hackathon from "@assets/Hackathon.png";
 import { forms } from "@assets/forms";
+import { outDoorItems } from "dummies/outDoorItems";
 
 interface CardProps {
   url: string;
@@ -18,11 +16,6 @@ interface CardProps {
   mode: "photo" | "editform";
   mr?: number;
 }
-const outDoorItems = [
-  { id: 1, title: "여자들끼리 바다여행", url: outdoorSleep1, likes: 20 },
-  { id: 2, title: "고등학교 친구들이랑 여행", url: outdoorSleep2, likes: 12 },
-  { id: 3, title: "대학교 친구들이랑 글램핑", url: outdoorSleep3, likes: 30 },
-];
 
 const ttsItems = ["안녕하세요 어머니!!", "잘 놀다 갈게요~", "감사합니다!!"];
 
@@ -58,7 +51,7 @@ export default function Home() {
       <section className="flex flex-col gap-2 px-4 mb-12">
         <Title title="외박 인증은 이 사진!" to="/photo" />
         <div className="flex flex-grow overflow-x-scroll">
-          {outDoorItems.map((item) => (
+          {outDoorItems.slice(0, 3).map((item) => (
             <Card mode="photo" mr={3} key={item.title} {...item} />
           ))}
         </div>
