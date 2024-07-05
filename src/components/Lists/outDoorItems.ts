0ac3e7c517d92a3f61c69f251c6f170ae7c40c42
JJHyeok13 <jjinh99@naver.com>
2024-07-05 09:44:36 +0900

    feat: 배포 준비

---
+++ src/components/Lists/outDoorItems.ts
@@ -0,0 +1,73 @@
+import outdoorSleep1 from "@assets/outdoor-sleep1.png";
+import friend1 from "@assets/friend.png";
+import friend2 from "@assets/friend2.png";
+import farm1 from "@assets/farm1.png";
+import farm2 from "@assets/farm2.png";
+import coding1 from "@assets/coding.jpeg";
+import coding2 from "@assets/coding2.jpeg";
+
+export const outDoorItems = [
+  {
+    title: "친구 생일파티 사진",
+    url: outdoorSleep1,
+    user: "우정킹",
+    likes: 20,
+    dislikeCount: 10,
+    downloadCount: 5,
+    id: 1,
+  },
+  {
+    title: "친구들과 한장",
+    url: friend1,
+    user: "발왕넓음",
+    likes: 12,
+    dislikeCount: 2,
+    downloadCount: 3,
+    id: 2,
+  },
+  {
+    title: "우정 사진",
+    url: friend2,
+    user: "바다여왕",
+    likes: 30,
+    dislikeCount: 5,
+    downloadCount: 10,
+    id: 3,
+  },
+  {
+    title: "농활 사진",
+    url: farm1,
+    user: "농활친",
+    likes: 20,
+    dislikeCount: 4,
+    downloadCount: 7,
+    id: 4,
+  },
+  {
+    title: "농활 사진입니다",
+    url: farm2,
+    user: "농활활",
+    likes: 30,
+    dislikeCount: 3,
+    downloadCount: 8,
+    id: 5,
+  },
+  {
+    title: "해커톤 하는 사진",
+    url: coding1,
+    user: "코딩신",
+    likes: 30,
+    dislikeCount: 6,
+    downloadCount: 9,
+    id: 6,
+  },
+  {
+    title: "해커톤 현장",
+    url: coding2,
+    user: "해커톤 중독",
+    likes: 30,
+    dislikeCount: 7,
+    downloadCount: 10,
+    id: 7,
+  },
+];
